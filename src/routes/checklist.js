b65d3d52546b66a6b781fff8a5e8e19caca7962f
Maisa <maisa.folgueral@gmail.com>
2023-04-05 17:03:48 -0300

    Updated put data

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -26,6 +26,17 @@ router.get("/new", async (req, res) => {
   }
 });
 
+router.get("/:id/edit", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", {
+      errors: "Erro ao exibir a edição de Lista de tarefas",
+    });
+  }
+});
+
 router.post("/", async (req, res) => {
   let { name } = req.body.checklist;
   let checklist = new Checklist({ name });
@@ -52,16 +63,17 @@ router.get("/:id", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  let { name } = req.body;
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
+
   try {
-    let checklist = await Checklist.findByIdAndUpdate(
-      req.params.id,
-      { name },
-      { new: true }
-    );
-    res.status(200).json(checklist);
+    await checklist.updateOne({ name });
+    res.redirect("/checklists");
   } catch (error) {
-    res.status(422).json(error);
+    let errors = error.errors;
+    res
+      .status(422)
+      .render("/checklists/edit", { checklist: { ...checklist, errors } });
   }
 });
 
