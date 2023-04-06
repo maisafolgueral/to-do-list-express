33ab68e15a2142106b3d9bb6d93742984f248959
Maisa <maisa.folgueral@gmail.com>
2023-04-06 14:06:58 -0300

    Created request JSON

--- src/routes/task.js
+++ src/routes/task.js
@@ -53,6 +53,18 @@ checklistsDependentRoute.post("/:id/tasks", async (req, res) => {
   }
 });
 
+simpleRouter.put("/:id", async (req, res) => {
+  let task = await Task.findById(req.params.id);
+  try {
+    task.set(req.body.task);
+    await task.save();
+    res.status(200).json({ task });
+  } catch (error) {
+    let errors = error.errors;
+    res.status(422).json({ task: { ...errors } });
+  }
+});
+
 module.exports = {
   checklistsDependent: checklistsDependentRoute,
   simple: simpleRouter,
